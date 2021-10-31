580421e7509e536234c664aa0f2da10af0575922
32248480 <92283556+32248480@users.noreply.github.com>
2021-10-31 11:54:51 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,28 +1,21 @@
-class Cbbb
-{
-	double x; 
-	double y;
-}
+class CBox
+ {
+ int length;
+ int width;
+ int height;
+ }
+ public class Main
+ {
+ public static void main(String arge[])
+ {
+ CBox box;
+ box=new CBox(); 
+ box.length=15;
+ box.width=10;
+ box.height=25;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Cbbb obj1=new Cbbb();
-		Cbbb obj2=new Cbbb();
-		Cbbb avg=new Cbbb();
-		
-		obj1.x=5.2;
-		obj1.y=3.9;
-		
-		obj2.x=6.5;
-		obj2.y=4.6;
-		
-		avg.x=(obj1.x+obj2.x)/2;
-		avg.y=(obj1.y+obj2.y)/2;
-		
-		System.out.println("obj1.x="+obj1.x+", obj1.y="+obj1.y);
-		System.out.println("obj2.x="+obj2.x+", obj2.y="+obj2.y);
-		System.out.println("avg.x="+avg.x+", avg.y="+avg.y);
-	}
-}
+ System.out.println("length= "+box.length);
+ System.out.println("width="+box.width);
+ System.out.println("height="+box.height);
+ }
+} 
