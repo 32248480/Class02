751a769cdb083b187d68be3161b8307b14d44fda
32248480 <92283556+32248480@users.noreply.github.com>
2021-11-15 03:50:40 +0800

    Update Class05.java

--- Class05.java
+++ Class05.java
@@ -1,43 +1,36 @@
-class CBox
-{
-int length;
-int width;
-int height;
+class CBox{
+ int length;
+ int width;
+ int height;
 
-int volume()
-{
-return length*width*height;
+int volume(){
+ return length*width*height;
 }
-int surfaceArea()
-{
-return 2*((length*width)+(width*height)+(length*height));
+int surfaceArea(){
+ return 2*((length*width)+(width*height)+(length*height));
 }
-void showData()
-{
-System.out.println("length="+length);
-System.out.println("width="+width);
-System.out.println("height="+height);
+void showData(){
+ System.out.println("length="+length);
+ System.out.println("width="+width);
+ System.out.println("height="+height);
 }
-void showAll()
-{
-showData();
-System.out.println("surface area="+surfaceArea());
-System.out.println("volume="+volume());
+void showAll(){
+ showData();
+ System.out.println("surface area="+surfaceArea());
+ System.out.println("volume="+volume());
 }
 }
 
-public class Main
-{
-public static void main(String args[])
-{
-CBox box;
-box=new CBox();
+public class Main{
+public static void main(String args[]){
+ CBox box;
+ box=new CBox();
 
-box.length=3;
-box.width=6;
-box.height=9;
+ box.length=3;
+ box.width=6;
+ box.height=9;
 
-box.showAll();
+ box.showAll();
 }
 }
 
